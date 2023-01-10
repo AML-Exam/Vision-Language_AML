c0f5b639352b7da8d6e4cdab18a259c61f982313
saccuz <52223431+saccuz@users.noreply.github.com>
2023-01-10 16:04:41 +0100

    First modification of domain_disentangle
    Add init function with the recon_loss
    Add save and load checkpoint
    Add train_iteration with label for category and domain. Evaluation of different losses

--- experiments/domain_disentangle.py
+++ experiments/domain_disentangle.py
@@ -1,17 +1,70 @@
+import torch
+from models.base_model import DomainDisentangleModel
 
-#TODO
 class DomainDisentangleExperiment: # See point 2. of the project
     
     def __init__(self, opt):
-        raise NotImplementedError('[TODO] Implement DomainDisentangleExperiment.')
+         # Utils
+        self.opt = opt
+        self.device = torch.device('cpu' if opt['cpu'] else 'cuda:0')
+
+        # Setup model
+        self.model = DomainDisentangleModel()
+        self.model.train()
+        self.model.to(self.device)
+        for param in self.model.parameters():
+            param.requires_grad = True
+
+        # Setup optimization procedure
+        # forse possiamo usare il gradient descend
+        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=opt['lr'])
+        self.criterion = torch.nn.CrossEntropyLoss()
+        self.recon_loss = torch.nn.MSELoss()
 
     def save_checkpoint(self, path, iteration, best_accuracy, total_train_loss):
-        raise NotImplementedError('[TODO] Implement DomainDisentangleExperiment.')
+        checkpoint = {}
+
+        checkpoint['iteration'] = iteration
+        checkpoint['best_accuracy'] = best_accuracy
+        checkpoint['total_train_loss'] = total_train_loss
+
+        checkpoint['model'] = self.model.state_dict()
+        checkpoint['optimizer'] = self.optimizer.state_dict()
+
+        torch.save(checkpoint, path)
 
     def load_checkpoint(self, path):
-        raise NotImplementedError('[TODO] Implement DomainDisentangleExperiment.')
+        checkpoint = torch.load(path)
+
+        iteration = checkpoint['iteration']
+        best_accuracy = checkpoint['best_accuracy']
+        total_train_loss = checkpoint['total_train_loss']
+
+        self.model.load_state_dict(checkpoint['model'])
+        self.optimizer.load_state_dict(checkpoint['optimizer'])
+
+        return iteration, best_accuracy, total_train_loss
 
     def train_iteration(self, data):
+        image, obj_label, dom_label = data
+
+        image = image.to(self.device)
+        obj_label = obj_label.to(self.device)
+        dom_label = dom_label.to(self.device)
+
+        features, obj_class, dom_class, recon_feat, adv_dom_to_obj_class, adv_obj_to_dom_class = self.model(image)
+        celoss_obj = self.criterion(obj_class, obj_label)
+        celoss_dom = self.criterion(dom_class, dom_label)
+        eloss_dom_to_obj = - self.criterion(adv_dom_to_obj_class, obj_label)
+        eloss_obj_to_dom = - self.criterion(adv_obj_to_dom_class, dom_label)
+        rec_loss = self.recon_loss(recon_feat, features)
+
+        total_loss = celoss_obj + celoss_dom + eloss_dom_to_obj + eloss_obj_to_dom + rec_loss
+        self.optimizer.zero_grad()
+        total_loss.backward()
+        self.optimizer.step()
+        
+        return total_loss.item()
         raise NotImplementedError('[TODO] Implement DomainDisentangleExperiment.')
 
     def validate(self, loader):

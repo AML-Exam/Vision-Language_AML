1c02203e7da7af226e1289250229485d33f927a6
saccuz <marco.sacchet.97@gmail.com>
2023-01-20 11:33:15 +0100

    reverse layer added

--- experiments/domain_disentangle.py
+++ experiments/domain_disentangle.py
@@ -70,7 +70,7 @@ class DomainDisentangleExperiment: # See point 2. of the project
             images, labels = data
             images = images.to(self.device)
             labels = labels.to(self.device)
-            features, rec_features, source_class_outputs, source_dom_outputs, source_adv_objC_outputs, source_adv_domC_outputs = self.model(images, True)
+            features, rec_features, source_class_outputs, source_dom_outputs, source_adv_objC_outputs, source_adv_domC_outputs = self.model(images, True, self.opt["alpha"])
             source_class_loss = weigths[0]*self.crossEntropyLoss(source_class_outputs, labels)
             print(f"source_class_loss: {source_class_loss.item()}")
             source_dom_loss = weigths[1]*self.crossEntropyLoss(source_dom_outputs, torch.zeros(self.opt['batch_size'], dtype = torch.long).to(self.device))
@@ -86,7 +86,7 @@ class DomainDisentangleExperiment: # See point 2. of the project
         else:
             images, _ = data
             images = images.to(self.device)
-            features, rec_features, _ , target_dom_outputs, target_adv_objC_outputs, target_adv_domC_outputs = self.model(images, True)
+            features, rec_features, _ , target_dom_outputs, target_adv_objC_outputs, target_adv_domC_outputs = self.model(images, True, self.opt["alpha"])
             target_dom_loss = weigths[0]*self.crossEntropyLoss(target_dom_outputs, torch.ones(target_dom_outputs.size()[0], dtype = torch.long).to(self.device))
             reconstruction_loss = weigths[2]*self.mseloss(rec_features, features)
             print("reconstruction_loss: ",reconstruction_loss.item())
